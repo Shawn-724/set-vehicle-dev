c8a571dd387dec823f66296eb4dfb33f52213374
Shawn-724 <youxiong.shawn.ye@gmail.com>
2024-11-24 11:18:20 -0600

    added undefined for all

---
+++ set-vehicle/src/components/MakeSelect.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { Select } from "antd";
+import axios from "axios";
+
+const { Option } = Select;
+
+interface MakeSelectProps {
+  year: number | undefined;
+  onMakeChange: (make: string | undefined) => void;
+}
+
+const MakeSelect: React.FC<MakeSelectProps> = ({ year, onMakeChange }) => {
+  const [makeList, setMakeList] = useState<string[]>([]);
+  const [selectedMake, setSelectedMake] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (!year) {
+      setMakeList([]);
+      setSelectedMake(undefined);
+      return;
+    }
+
+    const url = `https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForManufacturerAndYear/mer?year=${year}&format=json`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        const makeNames = Array.from(
+          new Set(
+            response.data.Results.map(
+              (item: { MakeName: string }) => item.MakeName
+            )
+          )
+        ) as string[];
+        setMakeList(makeNames);
+        setSelectedMake(undefined);
+      })
+      .catch((error) => {
+        console.error("Error fetching makes:", error);
+        setMakeList([]);
+      });
+  }, [year]);
+
+  const handleMakeChange = (value: string | undefined) => {
+    setSelectedMake(value);
+    onMakeChange(value);
+  };
+
+  return (
+    <Select
+      showSearch
+      placeholder="2 | Make"
+      value={selectedMake}
+      onChange={handleMakeChange}
+      style={{ width: 200 }}
+      disabled={!year}
+    >
+      {makeList.map((make) => (
+        <Option value={make}>{make}</Option>
+      ))}
+    </Select>
+  );
+};
+
+export default MakeSelect;
---
+++ set-vehicle/src/components/YearSelect.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Select } from "antd";
+
+const { Option } = Select;
+
+interface YearSelectProps {
+  year: number | undefined;
+  onYearChange: (value: number | undefined) => void;
+}
+
+const YearSelect: React.FC<YearSelectProps> = ({ year, onYearChange }) => {
+  const years = Array.from(
+    { length: 2023 - 1995 + 1 },
+    (_, index) => 2023 - index
+  );
+
+  return (
+    <Select
+      showSearch
+      placeholder="1 | Year"
+      value={year}
+      onChange={onYearChange}
+      style={{ width: 200 }}
+    >
+      {years.map((year) => (
+        <Option value={year}>{year}</Option>
+      ))}
+    </Select>
+  );
+};
+
+export default YearSelect;
